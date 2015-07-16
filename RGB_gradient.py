8abd0140f3a41ec0d8f309be9b6ecdae4f9896b9
chennessey3 <conor.hennessey@gmail.com>
2015-07-16 15:53:07 -0400

    Successful test plot

--- RGB_gradient.py
+++ RGB_gradient.py
@@ -2,6 +2,8 @@ import math
 
 print "Started"
 
+incs = 40
+
 def hex_to_rgb(value):
     value = value.lstrip('#')
     lv = len(value)
@@ -15,6 +17,7 @@ color_1 = (101,146,0)
 color_2 = (0,230,253)
 
 def color_gradient(color_1,color_2,incs): #for R,G,B touples and at least 2 incs
+    color_list= []
     delta_R = color_2[0] - color_1[0]
     delta_G = color_2[1] - color_1[1]   
     delta_B = color_2[2] - color_1[2] 
@@ -27,21 +30,22 @@ def color_gradient(color_1,color_2,incs): #for R,G,B touples and at least 2 incs
 #        print i 
         color_temp = rgb_to_hex((int(color_1[0] + inc_R*i ),int(color_1[1] + inc_G*i ),int(color_1[2] + inc_B*i )) )
         print color_temp
-    
-    return [(0,0,0),(0,230,253)]
+        color_list.append(color_temp)
+    return color_list
 
-color_gradient(color_1,color_2,20)
+
+
 
 
 import matplotlib.pyplot as plt
 
 
-x = [0,1,2,3]
+x = [x for x in range(incs)]
 y = x
 
 
 plt.axes()
-plt.scatter(x, y,s=900, linestyle='-', c=['#659200','#4aa842','#25c79f','#00e6fd'], marker="o",edgecolors='none')
+plt.scatter(x, y,s=90, linestyle='-', c=color_gradient(color_1,color_2,incs), marker="o",edgecolors='none')
 plt.axis('scaled')
 plt.show()
 
